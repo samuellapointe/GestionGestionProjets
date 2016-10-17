3440f42028efe343bf0c5ad62306529091717231
Knuk <samuellapointe@gmail.com>
2016-10-17 15:34:38 -0400

    Add and display task object

--- js/taskmanager.js
+++ js/taskmanager.js
@@ -1,40 +1,51 @@
 ﻿var tasks = new Array();
 
-function addTask() {
-	var task = {
-		name: "Test"
+function Task(name, estimatedTime) {
+	this.name = name;
+	this.estimatedTime = estimatedTime;
+
+	this.toString = function() {
+		return this.name + "<br\>" + "Temps accordé: " + estimatedTime + "h";
 	}
+}
+
+function addTask() {
+	var task = new Task("Créer un formulaire", 1.5);
 	tasks.push(task);
-	console.log("Test");
 	updateTasksLayout();
 }
 
 function updateTasksLayout() {
 	var container = document.getElementById("taskContainer");
 	container.innerHTML = ""; // Empty the container
-	
+
 	var nbTasks = tasks.length;
-	
+
 	// Add every task
 	for (var i = 0; i < nbTasks; i++) {
-		var task = createTaskElement(tasks[i].name, "taskInner");
+		var task = createTaskElement(tasks[i].toString(), false);
 		container.appendChild(task);
 	}
-	
+
 	// Add the "Add task" button
-	var addTaskButton = createTaskElement("Ajouter une tâche", "taskInner taskAdd");
+	var addTaskButton = createTaskElement("Ajouter une tâche", true);
 	container.appendChild(addTaskButton);
 }
 
-function createTaskElement(text, className) {
+// Si isAddTask = true, alors c'est le bouton d'ajout de tâche
+function createTaskElement(text, isAddTask) {
 	var outerDiv = document.createElement("div");
 	outerDiv.className = "taskOuter";
-	
+
 	var innerDiv = document.createElement("div");
-	innerDiv.className = className;
+	innerDiv.className = "taskInner";
 	innerDiv.innerHTML = text;
-	innerDiv.onclick = function() { addTask(); };
-	
+
+	if (isAddTask) {
+		innerDiv.className = "taskInner taskAdd";
+		innerDiv.onclick = function() { addTask(); };
+	}
+
 	outerDiv.appendChild(innerDiv);
 	return outerDiv;
 }
