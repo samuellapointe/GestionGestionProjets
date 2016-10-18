642f92b38805788d340da6a9f60e06578109ad97
rocko71 <potvinmarcandre@gmail.com>
2016-10-18 17:12:47 -0400

    delete des taches
    J'ai créer la suppression des tâches

--- js/taskmanager.js
+++ js/taskmanager.js
@@ -27,6 +27,19 @@ function addTask() {
 	tasks.push(task);
 	updateTasksLayout();
 }
+function deleteTask(name)
+{
+	var nbTasks = tasks.length;
+	
+	for (var i = 0; i < nbTasks; i++)
+	{
+		if(tasks[i].name == name)
+		{
+			tasks.splice(i,1);
+		}
+	}
+	updateTasksLayout();
+}
 
 function updateTasksLayout() {
 	var container = document.getElementById("taskContainer");
@@ -36,28 +49,30 @@ function updateTasksLayout() {
 
 	// Add every task
 	for (var i = 0; i < nbTasks; i++) {
-		var task = createTaskElement(tasks[i].toString(), false);
+		var task = createTaskElement(tasks[i].toString(),tasks[i].name, false);
 		container.appendChild(task);
 	}
 
 	// Add the "Add task" button
-	var addTaskButton = createTaskElement("Ajouter une tâche", true);
+	var addTaskButton = createTaskElement("Ajouter une tâche", "", true);
 	container.appendChild(addTaskButton);
 }
 
 // Si isAddTask = true, alors c'est le bouton d'ajout de tâche
-function createTaskElement(text, isAddTask) {
+function createTaskElement(text, taskName, isAddTask) {
 	var outerDiv = document.createElement("div");
 	outerDiv.className = "taskOuter";
 
 	var innerDiv = document.createElement("div");
 	innerDiv.className = "taskInner";
 	innerDiv.innerHTML = text;
+	innerDiv.onclick = function() { deleteTask(taskName);};
 
 	if (isAddTask) {
 		innerDiv.className = "taskInner taskAdd";
 		innerDiv.onclick = function() { addTask(); };
 	}
+	
 
 	outerDiv.appendChild(innerDiv);
 	return outerDiv;
