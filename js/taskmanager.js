3430c09613b5c387969fa6a3a9969f57f637290c
Valtar David <Valtar David>
2016-10-23 21:08:43 -0400

    Export/Import des taches en fichier XML

--- js/taskmanager.js
+++ js/taskmanager.js
@@ -77,3 +77,85 @@ function createTaskElement(text, taskName, isAddTask) {
 	outerDiv.appendChild(innerDiv);
 	return outerDiv;
 }
+
+function ExportToXML(){
+	if(tasks.length != 0)
+	{
+		var a = document.createElement("a"),
+        file = new Blob([formatTasksToXml()], {type: 'text/plain;charset=utf-8'});
+		if (window.navigator.msSaveOrOpenBlob) // Internet Explorer
+        window.navigator.msSaveOrOpenBlob(file, "Taches.xml");
+		else//Reste
+		{
+			a.href = window.URL.createObjectURL(file);
+			a.download = "Taches.xml";
+			document.body.appendChild(a);
+			a.click();
+			setTimeout(function() {
+				document.body.removeChild(a);
+				window.URL.revokeObjectURL(url);  
+			}, 0); 
+		}
+	}
+	else
+	{
+		alert("Vous devez avoir au moins une tâche!");
+	}
+}
+
+function importXML(){
+	var leFichierXml = document.getElementById("MonFichier");
+	if(leFichierXml.value != '')
+	{
+		var file = leFichierXml.files[0];
+		var textType = /xml.*/;
+		
+		if(file.type.match(textType))
+		{
+			var reader = new FileReader();
+			reader.onload = function(e){
+				formatXMLToTask(reader.result);
+			}
+			reader.readAsText(file);
+		}
+		else
+			alert("Choisir un fichier XML!");
+	}
+	else
+	{
+		alert("Choisir un fichier avant");
+	}
+	
+	
+}
+
+function formatTasksToXml(){
+	var fichierXml = "<?xml version =\"1.0\" ?>\n<listeTaches>";
+	for(var i = 0; i < tasks.length; i++)
+	{
+		fichierXml+="\n\t<Tache>";
+		fichierXml+="\n\t\t<nomTache>" + tasks[i].name + "</nomTache>";
+		fichierXml+="\n\t\t<tempEstime>" + tasks[i].estimatedTime + "</tempEstime>";
+		fichierXml+="\n\t</Tache>";
+	}
+	fichierXml+="\n</listeTaches>";
+	return fichierXml;
+}
+
+function formatXMLToTask(fileResults){
+	var lines = fileResults.split('\n');
+	for(var i = 3; i < lines.length; i++)
+	{
+		var taskName = lines[i].split('>')[1].split('<')[0];
+		var taskTime = lines[i+1].split('>')[1].split('<')[0];
+		
+		tasks.push(new Task(taskName, taskTime));
+
+		if(lines[i+3] == "\t<Tache>")
+			i+=3;
+		else
+			break;
+	}
+	
+	updateTasksLayout();
+}
